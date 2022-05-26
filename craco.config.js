a365115e8d636910c89f8c72c56ee9219b4a1c27
huangyan321 <1604549268@qq.com>
2022-05-26 14:58:33 +0800

    add:使用croco 渗透webpack配置

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
+
 const resolve = (dir) => path.resolve(__dirname, dir);
-module.export = {
+module.exports = {
   webpack: {
     alias: {
-      '@': resolve('@'),
-    },
+      '@': resolve('src')
+    }
   },
 };
