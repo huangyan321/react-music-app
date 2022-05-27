3f50dd707f89f37ef9051e231e6fb1b8e3bb599d
huangyan321 <1604549268@qq.com>
2022-05-27 09:48:48 +0800

    add:header

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -1,15 +1,63 @@
 import React, { memo } from 'react';
-import { NavLink } from 'react-router-dom'
-import { HeaderWrapper } from './style';
+import { NavLink } from 'react-router-dom';
+import { HeaderWrapper, HeaderLeft, HeaderRight } from './style';
+import { headerLinks } from '@/common/local-data';
+import { Input, Button } from 'antd';
+const { Search } = Input;
 const MusicHeader = memo(() => {
+  function renderLink(route) {
+    if (!route.external) {
+      return (
+        <li key={route.title}>
+          <NavLink to={route.link}>
+            <em>{route.title}</em>
+            <div className="arrow sprite_01">&nbsp;</div>
+          </NavLink>
+        </li>
+      );
+    }
+    return (
+      <li key={route.title}>
+        <a href={route.link}>{route.title}</a>
+        <div className="arrow sprite_01">&nbsp;</div>
+      </li>
+    );
+  }
+  const onSearch = (value) => console.log(value);
   return (
-    <div>
-      <HeaderWrapper>
-        <NavLink to="/">发现音乐</NavLink>
-        <NavLink to="/mine">我的音乐</NavLink>
-        <NavLink to="/friends">关注</NavLink>
-      </HeaderWrapper>
-    </div>
+    <HeaderWrapper>
+      <div className="header_bar">
+        <div className="wrap-v1 content">
+          <HeaderLeft>
+            <h1 className="logo sprite_01">
+              <a href="/#">logo</a>
+            </h1>
+            <ul className="select_title">
+              {headerLinks.map((route) => {
+                return renderLink(route);
+              })}
+            </ul>
+          </HeaderLeft>
+          <HeaderRight>
+            <Search
+              placeholder="input search text"
+              allowClear
+              onSearch={onSearch}
+              style={{
+                width: 200,
+              }}
+            />
+            <Button className="creator" ghost>
+              创作者中心
+            </Button>
+            <Button className="login_btn" type="text">
+              登录
+            </Button>
+          </HeaderRight>
+        </div>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
   );
 });
 
--- src/components/app-header/style.js
+++ src/components/app-header/style.js
@@ -1,4 +1,90 @@
-import style from 'styled-components';
-export const HeaderWrapper = style.div`
-font-size: 22px;
-`
+import styled from 'styled-components';
+import sprite_01 from '@/assets/img/sprite_01.png';
+export const HeaderWrapper = styled.div`
+  font-size: 22px;
+  .header_bar {
+    height: 70px;
+    background-color: #242424;
+    .content {
+      height: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+  .divider {
+    height: 5px;
+    background-color: #b2281e;
+  }
+`;
+export const HeaderLeft = styled.div`
+  display: flex;
+  .logo {
+    width: 176px;
+    height: 69px;
+    text-indent: -9999px;
+    cursor: pointer;
+    //可使用该方式导入背景图片
+    /* background: url(${sprite_01}) no-repeat 0 9999px; */
+    background-position: 0 0;
+  }
+  .select_title {
+    height: 100%;
+    display: flex;
+    position: relative;
+    a {
+      height: 100%;
+      display: block;
+      line-height: 70px;
+      padding: 0 20px;
+      font-size: 14px;
+      text-decoration: none;
+      position: relative;
+    }
+    .active {
+      background-color: #000000;
+      color: #ffffff;
+      .arrow {
+        position: absolute;
+        width: 12px;
+        height: 7px;
+        left: 50%;
+        margin-left: -6px;
+        background-position: -226px 0;
+        overflow: hidden;
+        top: 64px;
+      }
+    }
+    a:hover {
+      background-color: #000000;
+      color: #ffffff;
+    }
+    li {
+      position: relative;
+      :last-of-type {
+        ::after {
+          content: '';
+          position: absolute;
+          top: 21px;
+          left: 100px;
+          width: 28px;
+          height: 19px;
+          background-position: -190px 0;
+          background-image: url(${sprite_01});
+          background-repeat: no-repeat;
+        }
+      }
+    }
+  }
+`;
+export const HeaderRight = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  > span,
+  > button {
+    margin: 0 5px;
+  }
+  .ant-input-search {
+    width: 159px !important;
+  }
+`;
