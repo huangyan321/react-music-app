25a1c9722a50a07c6525cc1e6a1a93ae3c02e605
huangyan321 <1604549268@qq.com>
2022-05-29 22:04:34 +0800

    add:推荐页路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,62 @@
-import Discover from '@/views/discover'
-import Mine from '@/views/mine'
-import Friends from '@/views/friends'
+import { Navigate } from 'react-router-dom';
 
+import Discover from '@/views/discover';
+import Album from '@/views/discover/com-pages/album';
+import New from '@/views/discover/com-pages/new';
+import Radio from '@/views/discover/com-pages/radio';
+import Top from '@/views/discover/com-pages/top';
+import Recommend from '@/views/discover/com-pages/recommend';
+import Singer from '@/views/discover/com-pages/singer';
+
+import Mine from '@/views/mine';
+import Friends from '@/views/friends';
 const routes = [
   {
     path: '/',
     exact: true,
-    element: <Discover />
+    element: <Navigate to="/discover/recommend" />,
+  },
+  {
+    path: '/discover',
+    element: <Discover />,
+    children: [
+      {
+        path: '/discover',
+        element: <Navigate to="recommend" />,
+      },
+      {
+        path: 'recommend',
+        element: <Recommend />,
+      },
+      {
+        path: 'album',
+        element: <Album />,
+      },
+      {
+        path: 'new',
+        element: <New />,
+      },
+      {
+        path: 'radio',
+        element: <Radio />,
+      },
+      {
+        path: 'top',
+        element: <Top />,
+      },
+      {
+        path: 'singer',
+        element: <Singer />,
+      },
+    ],
   },
   {
     path: '/mine',
-    element: <Mine />
+    element: <Mine />,
   },
   {
     path: '/friends',
-    element: <Friends />
-  }
-]
-export default routes
+    element: <Friends />,
+  },
+];
+export default routes;
